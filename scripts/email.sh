d0e2850d8874ce3a02ff2be4c242b555c31254ce
root <root@gis-lab-VROautomationtesting.gis-lab.sms.local>
2020-06-19 16:05:48 +0530

    change in path -> email.sh

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,3 +1,3 @@
 echo "Sending precheck validation report of Servicenow Ticket which are currently in queue:"
 $(cd /var/lib/awx/projects/_18__git_expansion_demo/reports/ ; cat RITM* > ./meregedreport.txt )
-mailx  -s "Precheck validation reports $i" -S smtp="mailhub.lss.emc.com" "nishu.prakash@emc.com"</var/lib/awx/projects/_18__git_expansion_demo/reports/meregedreport.txt
+mailx  -s "Precheck validation reports $i" -S smtp="mailhub.lss.emc.com" "nishu.prakash@emc.com"</var/lib/awx/projects/_18__git_expansion_demo/reports/*
